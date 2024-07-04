e3b098c507780a0d3a1faa796b56774b7c578bbe
Shabnam <140334239+shab-am@users.noreply.github.com>
2024-07-05 04:12:18 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+var tryItNow = document.getElementById("tryItNow");
+tryItNow.onclick(() => {
+    windows.open("https://partyrock.aws/u/muskangoyal/cp2TNnD6I/Empower-AI%3A-Your-Comprehensive-Investment-Companion", "_blank");
+   
+})
